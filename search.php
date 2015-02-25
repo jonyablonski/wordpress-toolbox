84ebe72f1553521783048d5f8b30ea78766dee4e
Jon Yablonski <jon@thinkmoncur.com>
2015-02-25 16:49:33 -0500

    search

---
+++ search.php
@@ -0,0 +1,5 @@
+<div id="search" class="sidebar__search">
+	<form id="searchform" action="<?php echo site_url(); ?>" method="get">
+		<input type="text" name="s" id="s" size="15" placeholder="Search the Blog..." class="search-input" />
+	</form>
+</div>
